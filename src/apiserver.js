825aed76a2a25c159bcd5079f7e4739da41cffed
Dan Foad <danielfoad@gmail.com>
2018-04-26 22:53:55 +0100

    Added express server for api

---
+++ src/apiserver.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const app = express();
+
+const bodyParser = require('body-parser');
+const path = require('path');
+
+app.use(express.static(path.join(__dirname, 'build')));
+app.use(bodyParser.json());
+
+app.get('/api/getRequest', (req, res) => {
+    res.send(JSON.stringify({ complete: true }));
+});
+
+app.post('/api/postRequest', (req, res) => {
+//API logic
+});
+
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, 'build/index.html'));
+});
+
+const port = process.env.PORT || 3001;
+app.listen(port, () => {
+    console.log('Listening on port', port);
+});
